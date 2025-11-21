081be7d9298c43d1b5859a3aa0b5de3c9bb4443e
p47n <patelmuhammed7777@gmail.com>
2025-11-21 00:41:24 -0330

    feat(profile): add UserProfile model and Firestore user service

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,35 @@
+class UserProfile {
+  final String uid;
+  final String name;
+  final String major;
+  final List<String> skillsTeach;
+  final List<String> skillsLearn;
+
+  UserProfile({
+    required this.uid,
+    required this.name,
+    required this.major,
+    required this.skillsTeach,
+    required this.skillsLearn,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'name': name,
+      'major': major,
+      'skillsTeach': skillsTeach,
+      'skillsLearn': skillsLearn,
+    };
+  }
+
+  factory UserProfile.fromMap(Map<String, dynamic> map) {
+    return UserProfile(
+      uid: map['uid'],
+      name: map['name'],
+      major: map['major'],
+      skillsTeach: List<String>.from(map['skillsTeach']),
+      skillsLearn: List<String>.from(map['skillsLearn']),
+    );
+  }
+}
